c3075a171b48ec6bd55dcf4073f1847634ce7567
TiooDark <mvieira3845@gmail.com>
2025-07-02 16:13:22 -0300

    wip

--- app/metodoForEach.js
+++ app/metodoForEach.js
@@ -1,10 +1,16 @@
 const elementoParaInserirLivros = document.getElementById('livros')
+const elementoComValorTotalDeLivrosDisponiveis = document.getElementById('valor_total_livros_disponiveis')
 
 function exibirOsLivrosNaTela(listaDeLivros){
+    elementoComValorTotalDeLivrosDisponiveis.innerHTML = ''
+    elementoParaInserirLivros.innerHTML = ''
     listaDeLivros.forEach(livro => {
+        // let disponibilidade = verificarDisponobilidadeDoLivro(livro)
+         let disponibilidade = livro.quantidade > 0 ? 'livro__imagens' : 'livro__imagens indisponivel'
+
     elementoParaInserirLivros.innerHTML += `
         <div class="livro">
-            <img class="livro__imagens" src="${livro.imagem}" alt="${livro.alt}" />
+            <img class="${disponibilidade}" src="${livro.imagem}" alt="${livro.alt}" />
                 <h2 class="livro__titulo">
                     ${livro.titulo}
                 </h2>
@@ -17,3 +23,11 @@ function exibirOsLivrosNaTela(listaDeLivros){
    `
     })
 }
+
+// function verificarDisponobilidadeDoLivro(livro){
+//             if (livro.quantidade > 0){
+//                 return 'livro__imagens'
+//             } else {
+//                 return 'livro__imagens indisponivel'
+//             }
+//         }
